08f8b4e690042985ad182b23ba7d096688cbec38
Michael Liu <michael.liu@rackspace.com>
2014-06-04 19:09:32 -0400

    adventure start. join start

--- dm.js
+++ dm.js
@@ -5,12 +5,30 @@ var dm = require('./init.js')
 var config = require('./config.js');
 var ch = config.options.channels[0];
 var chListener = 'message' + ch;
+var adventure = false;
 
-//Listen for game start calls
-console.log("Adding game start listeners");
+//Connected. Breaks for some reason.
+// dm.connect(function(){
+//   console.log('Connected');
+// });
 
-dm.addListener(chListener, function(from, message){
-  dm.say(ch, "I'M ALIVE!");
+//Introduce
+dm.once(chListener, function(from, message){
+  dm.say(ch, "Greetings adventurers! I can help you embark on a legendary quest. Type in .adventure to start one! Type .help to learn more.");
 });
 
-console.log("Ready for input");
+//Adding channel message listener
+dm.addListener(chListener, function(from, message){
+  //Listen for game start
+  if(!adventure && message.indexOf('.adventure') > -1){
+    console.log(from + " is starting a game");
+    dm.say(ch, from + " has decided to lead an adventure, which will embark in 5 minutes! Anyone wishing to join type .join");
+    adventure = true;
+  }
+
+  //Listen for joins
+  else if(adventure && message.indexOf('.join') > -1){
+    console.log(from + " joins");
+    dm.say(ch, from + " rises for the quest! Type .join to do the same.");
+  }
+});

5ae387e5b8867d90472f95f1c13941645230eebc
Michael Liu <michael.liu@rackspace.com>
2014-06-05 17:22:10 -0400

    debugging count

--- js/api.js
+++ js/api.js
@@ -9,13 +9,24 @@ exports.newPlayer = function(req, res){
     if(!err){
       var player = collection.find({ "name": name }).limit(1);
       player.count(function(err, count){
-        if(count > 0){
-          console.log("Found existing player");
-          return res.send(JSON.stringify([{channel: name, message: "You already have an existing character. If you would like to delete this character please type .delete"}]));
-        } else {
-          console.log("Creating new player");
+        if(!err){
+          if(count > 0){
+            console.log("Found existing player");
+            return res.send(JSON.stringify([{channel: name, message: "You already have an existing character. If you would like to delete this character please type .delete"}]));
+          } else {
+            console.log("Creating new player");
+            collection.insert({ name: name } , { safe: true } , function(err, obj){
+              if(!err){
+                return res.send(JSON.stringify([{channel: name, message: "Not ready yet"}]));
+              } else {
+                console.log("ERROR IN NEWPLAYER INSERT")
+              }
+            });
+          }
         }
       });
+    } else {
+      console.log("ERROR IN NEWPLAYER COLLECTION");
     }
   });
 };
@@ -29,15 +40,21 @@ exports.deletePlayer = function(req, res){
       var player = collection.find({ "name": name });
       console.log(player);
       player.count(function(err, count){
-        console.log(count);
-        if(count == null){
-          console.log("No player found");
-          return res.send(JSON.stringify([{channel: name, message: "You don't have a character for your account! Type .generate to make one."}]));
+        if(!err){
+          console.log(count);
+          if(count == null){
+            console.log("No player found");
+            return res.send(JSON.stringify([{channel: name, message: "You don't have a character for your account! Type .generate to make one."}]));
+          } else {
+            collection.remove({ name: name }, true);
+            return res.send(JSON.stringify([{channel: name, message: "Your character has been deleted. Type .generate to make a new one!"}]));
+          }
         } else {
-          collection.remove({ name: name }, true);
-          return res.send(JSON.stringify([{channel: name, message: "Your character has been deleted. Type .generate to make a new one!"}]));
+          console.log("ERROR IN DELETEPLAYER COUNT");
         }
       });
+    } else {
+      console.log("ERROR IN DELETEPLAYER COLLECTION")
     }
   });
 };
@@ -48,35 +65,47 @@ exports.joinGame = function(req, res){
   console.log("Creating new game");
   db.collection('players', function(err, collection){
     if(!err){
-      var player = collection.find({ name: name });
+      var player = collection.find({ "name": name });
       player.count(function(err, count){
-        if(count > 0){
-          //Use player in database
-          var playerId = player.at(0)._id;
-          db.collection('games', function(err, collection){
-            if(!err){
-              console.log("finding game and sorting");
-              var mostRecentGame = collection.find({}).sort({_id:-1}).limit(1);
-              console.log("sending back");
-              mostRecentGame.count(function(err, count){
-                if(count > 0 && mostRecentGame.active){
-                  //Insert player into database
-                  return res.send(JSON.stringify([{channel: req.body.channel, message: name + "rises for the quest!"}]));
-                } else {
-                  //Should the players all be their own row in this database
-                  //Or should players have a lot of games?
-                  // collection.insert({gameNumber : 0, player : })
-                  return res.send(JSON.stringify([{channel: req.body.channel, message: name + " has decided to lead an adventure, which will embark in 5 minutes! Anyone wishing to join type .adventure"}]));
-                }
-              });
-            }
-          });
+        if(!err){
+          if(count > 0){
+            //Use player in database
+            var playerId = player.at(0)._id;
+            db.collection('games', function(err, collection){
+              if(!err){
+                console.log("finding game and sorting");
+                var mostRecentGame = collection.sort({_id:-1}).limit(1);
+                console.log("sending back");
+                mostRecentGame.count(function(err, count){
+                  if(!err){
+                    if(count > 0 && mostRecentGame.active){
+                      //Insert player into database
+                      return res.send(JSON.stringify([{channel: req.body.channel, message: name + "rises for the quest!"}]));
+                    } else {
+                      //Should the players all be their own row in this database
+                      //Or should players have a lot of games?
+                      // collection.insert({gameNumber : 0, player : })
+                      return res.send(JSON.stringify([{channel: req.body.channel, message: name + " has decided to lead an adventure, which will embark in 5 minutes! Anyone wishing to join type .adventure"}]));
+                    }
+                  } else {
+                    console.log("ERROR IN JOINGAME GAMECOUNT");
+                  }
+                });
+              } else {
+                console.log("ERROR IN JOINGAME COLLECTION")
+              }
+            });
+          } else {
+            //Create new player
+            console.log("Sending player gen request back");
+            return res.send(JSON.stringify([{channel: req.body.channel, message: "Please create your character before joining. Check PM for more info."}, {channel: req.body.name, message: "To begin character generation, please type .generate"}]));
+          }
         } else {
-          //Create new player
-          console.log("Sending player gen request back");
-          return res.send(JSON.stringify([{channel: req.body.channel, message: "Please create your character before joining. Check PM for more info."}, {channel: req.body.name, message: "To begin character generation, please type .generate"}]));
+          console.log("ERROR IN JOINGAME PLAYERS COUNT");
         }
       });
+    } else {
+      console.log("ERROR IN JOINGAME PLAYERS COLLECTION");
     }
   });
 };
--- js/config.js
+++ js/config.js
@@ -1,38 +1,38 @@
 // Create the configuration
+// var config = {
+//   server: "irc.rackspace.com",
+//   botName: "DungeonMaster",
+//   host: "http://localhost",
+//   port: "3000",
+//   options: {
+//     userName: 'mich7532',
+//     password: '',
+//     channels: ['#test'],
+//     port: "6697",
+//     sasl: true,
+//     secure: true,
+//     floodProtection: false,
+//     autoRejoin: true,
+//     autoConnect: true,
+//     debug: true
+//   }
+// };
+
 var config = {
-  server: "irc.rackspace.com",
+  server: "irc.freenode.net",
   botName: "DungeonMaster",
   host: "http://localhost",
-  port: "3000",
+  port:"3000",
   options: {
-    userName: 'mich7532',
-    password: '',
-    channels: ['#test'],
-    port: "6697",
-    sasl: true,
-    secure: true,
+    channels: ['#dm-test'],
+    port: "6667",
+    sasl: false,
     floodProtection: false,
     autoRejoin: true,
     autoConnect: true,
     debug: true
   }
 };
-
-// var config = {
-//   server: "irc.freenode.net",
-//   botName: "DungeonMaster",
-//   host: "http://localhost",
-//   port:"3000",
-//   options: {
-//     channels: ['#dm-test'],
-//     port: "6667",
-//     sasl: false,
-//     floodProtection: false
-//     autoRejoin: true,
-//     autoConnect: true,
-//     debug: true
-//   }
-// };
 
 
 module.exports = config;

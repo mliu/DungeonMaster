a96ace021ce8059c8e9e2c5442a4e6be0ab5ed59
Michael Liu <michael.liu@rackspace.com>
2014-06-23 15:50:49 -0400

    moved functions in object to prototype

--- js/adventure.js
+++ js/adventure.js
@@ -10,26 +10,22 @@ Adventure = function(){
   this.playerCount = PARTYMAX;
   this.game = null;
   this.boss = null;
+}
 
-  function startAdventure(channel){
-    console.log("start adventure");
-    var gpColl = db.collection('gpRouting');
-    var gamesColl = db.collection('games');
-    var monsterColl = db.collection('monsters');
-    var gmRouting = db.collection('gmRouting');
-    var game = gamesColl.find({}).sort({_id:-1}).toArray()[0];
-    this.game = game;
-    //Generate boss
-    monsterColl.insert({ boss: Boss() }, {w: 1}, function(err, obj){
-      gmRouting.insert({ game: game._id, monster: obj._id });
-      this.boss = boss;
-    });
-    //TODO: Generate location nodes
-    //Generate adventure embark dialog
-    return JSON.stringify[{ channel: channel, message: "QUEST: Seek out and kill " + boss.legend }];
-  }
-
-  function genEvent(){
-
-  }
+Adventure.prototype.startAdventure = function (channel) {
+  console.log("start adventure");
+  var gpColl = db.collection('gpRouting');
+  var gamesColl = db.collection('games');
+  var monsterColl = db.collection('monsters');
+  var gmRouting = db.collection('gmRouting');
+  var game = gamesColl.find({}).sort({_id:-1}).toArray()[0];
+  this.game = game;
+  //Generate boss
+  monsterColl.insert({ boss: Boss() }, {w: 1}, function(err, obj){
+    gmRouting.insert({ game: game._id, monster: obj._id });
+    this.boss = boss;
+  });
+  //TODO: Generate location nodes
+  //Generate adventure embark dialog
+  return JSON.stringify[{ channel: channel, message: "QUEST: Seek out and kill " + boss.legend }];
 }

01e17c0ac5de403fb45686016cb40a4fbd9a2108
Michael Liu <michael.liu@rackspace.com>
2014-07-25 15:31:03 -0400

    dumping work computer changes

--- js/adventure.js
+++ js/adventure.js
@@ -8,11 +8,7 @@ require('./boss.js');
 // require('./monster.js');
 
 Adventure = function () {
-  //Default playercount to max party size
-  this.playerCount = PARTYMAX;
-  this.game = null;
-  this.boss = null;
-  this.map = null;
+
 }
 
 Adventure.prototype.startAdventure = function (channel, callback) {
@@ -23,21 +19,21 @@ Adventure.prototype.startAdventure = function (channel, callback) {
   var monsterColl = db.collection('monsters');
   var gmRouting = db.collection('gmRouting');
   gamesColl.find().sort({_id:-1}).toArray(function(err, arr){
-    ref.game = arr[0];
+    var game = arr[0];
     //Generate boss
-    ref.boss = new Boss();
-    monsterColl.insert({ boss: ref.boss }, {w: 1}, function(err, obj){
-      gmRouting.insert({ game: ref.game._id, monster: obj._id });
+    game.boss = new Boss();
+    monsterColl.insert({ boss: game.boss }, {w: 1}, function(err, obj){
+      gmRouting.insert({ game: game._id, monster: obj._id });
       //TODO: Generate location nodes
-      ref.generateMap();
+      ref.generateMap(game);
       //Generate adventure embark dialog
-      callback(JSON.stringify([{ channel: channel, message: 'QUEST: Seek out and kill ' + ref.boss.legend }]));
+      callback(JSON.stringify([{ channel: channel, message: 'QUEST: Seek out and kill ' + game.boss.legend }]));
     });
   });
 }
 
-Adventure.prototype.generateMap = function () {
-  console.log("generating map[");
+Adventure.prototype.generateMap = function (game) {
+  console.log("generating map");
   for (i = 0; i < MAPSIZE; i++) {
     for (j = 0; j < MAPSIZE; j++) {
 
